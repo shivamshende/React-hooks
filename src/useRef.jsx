5ba0efe2688819fb50a6b44285da02549a59b6e4
Shivam Shende <Shivamshende200@gmail.com>
2024-02-01 13:30:58 +0530

    fix - Added onChange handler to the input field to address the

--- src/useRef.jsx
+++ src/useRef.jsx
@@ -21,12 +21,19 @@ export default function UseRef() {
         refElement.current.style.color = "orange"
     }
 
+    const handleChange = (e) => {
+      setName(e.target.value);
+    };
+    
+
   return (
     <div>
         <h1>useRef</h1>
-        <input type="text" value={Name} ref= {refElement}/>
+        <input type="text" value={Name} onChange={handleChange} ref= {refElement}/>
         <button onClick={changeName}>Change Name</button>
       <button onClick={changeColor}>Change Color</button>
     </div>
   )
 }
+
+//onChange:-  When using 'value' in a controlled component, React expects you to handle changes to the input value by supplying an 'onChange' function.
